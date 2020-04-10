7a01c1abc55c172129ae9cfef6a15084317072f9
Niranjanct424 <niranjangyadav124@gmail.com>
2020-04-10 23:05:33 +0530

    modified in note component

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -15,31 +15,7 @@ export class NoteComponent implements OnInit {
   @Input() note: Note;
   isPinned: boolean;
 
-  // colorsList = [
-  //   [
-  //     { 
-  //       colorCode: "rgba(198, 222, 255,1)", name: "Blue" 
-  //     },
-  //     { 
-  //       colorCode: "rgba(229, 228, 226,1)", name: "Gray" 
-  //     },
-  //     { 
-  //       colorCode: "rgba(230, 169, 236,1)", name: "Pink" 
-  //     },
-  //   ],
-  //   [
-  //     { 
-  //       colorCode: "rgba(233, 171, 23,1)", name: "Yellow" 
-  //     },
-  //     { 
-  //       colorCode: "rgba(249, 150, 107,1)", name: "Orange" 
-  //     },
-  //     { 
-  //       colorCode: "rgba(255,255,255,1)", name: "white" 
-  //     },
-  //   ]
-  // ]
-
+  
   constructor(private noteService:NoteService,
     private matSnackBar: MatSnackBar,
     private _router: Router,
